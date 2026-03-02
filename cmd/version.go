9213e2801c0d5fdbf8d71ddb1d8bfe7058d70610
hisao <jaime@hisao.org>
2026-03-02 01:30:32 -0800

    Add version command

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Long:  "Print the version number of whooper",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(rootCmd.Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
